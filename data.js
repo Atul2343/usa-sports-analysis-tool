5591c30a28eea34d8e75937868c96b0c1bc3bd49
Atul2343 <Baadalgoswami975@gmail.com>
2026-01-19 23:16:48 +0530

    Add sports data for NBA, NFL, MLB, and NHL

--- data.js
+++ data.js
@@ -1,1 +1,48 @@
 
+const sportsData = {
+
+  NBA: {
+    homeBonus: 6,
+    teams: {
+      "Lakers": 88,
+      "Warriors": 86,
+      "Celtics": 90,
+      "Bulls": 80,
+      "Heat": 84
+    }
+  },
+
+  NFL: {
+    homeBonus: 7,
+    teams: {
+      "Chiefs": 92,
+      "49ers": 90,
+      "Eagles": 89,
+      "Cowboys": 87,
+      "Bills": 88
+    }
+  },
+
+  MLB: {
+    homeBonus: 5,
+    teams: {
+      "Yankees": 90,
+      "Dodgers": 92,
+      "Red Sox": 85,
+      "Astros": 88,
+      "Mets": 84
+    }
+  },
+
+  NHL: {
+    homeBonus: 5,
+    teams: {
+      "Bruins": 91,
+      "Avalanche": 90,
+      "Rangers": 88,
+      "Lightning": 89,
+      "Oilers": 87
+    }
+  }
+
+};
