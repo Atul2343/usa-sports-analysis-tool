f9d28500895c9ddb50aa22bbc721ef94a024e571
Atul2343 <Baadalgoswami975@gmail.com>
2026-01-19 23:44:34 +0530

    Refactor sportsData structure to include lastMatches

--- data.js
+++ data.js
@@ -1,48 +1,42 @@
-
 const sportsData = {
-
   NBA: {
     homeBonus: 6,
     teams: {
-      "Lakers": 88,
-      "Warriors": 86,
-      "Celtics": 90,
-      "Bulls": 80,
-      "Heat": 84
+      "Lakers": { strength: 88, lastMatches: [1,0,1] },
+      "Warriors": { strength: 86, lastMatches: [0,1,1] },
+      "Celtics": { strength: 90, lastMatches: [1,1,1] },
+      "Bulls": { strength: 80, lastMatches: [0,0,1] },
+      "Heat": { strength: 84, lastMatches: [1,0,0] }
     }
   },
-
   NFL: {
     homeBonus: 7,
     teams: {
-      "Chiefs": 92,
-      "49ers": 90,
-      "Eagles": 89,
-      "Cowboys": 87,
-      "Bills": 88
+      "Chiefs": { strength: 92, lastMatches:[1,1,0] },
+      "49ers": { strength: 90, lastMatches:[0,1,1] },
+      "Eagles": { strength: 89, lastMatches:[1,0,1] },
+      "Cowboys": { strength: 87, lastMatches:[0,0,1] },
+      "Bills": { strength: 88, lastMatches:[1,1,1] }
     }
   },
-
   MLB: {
     homeBonus: 5,
     teams: {
-      "Yankees": 90,
-      "Dodgers": 92,
-      "Red Sox": 85,
-      "Astros": 88,
-      "Mets": 84
+      "Yankees": { strength:90, lastMatches:[1,0,1] },
+      "Dodgers": { strength:92, lastMatches:[1,1,1] },
+      "Red Sox": { strength:85, lastMatches:[0,1,0] },
+      "Astros": { strength:88, lastMatches:[1,0,1] },
+      "Mets": { strength:84, lastMatches:[0,0,1] }
     }
   },
-
   NHL: {
-    homeBonus: 5,
+    homeBonus:5,
     teams: {
-      "Bruins": 91,
-      "Avalanche": 90,
-      "Rangers": 88,
-      "Lightning": 89,
-      "Oilers": 87
+      "Bruins": { strength:91, lastMatches:[1,1,0] },
+      "Avalanche": { strength:90, lastMatches:[1,0,1] },
+      "Rangers": { strength:88, lastMatches:[0,1,0] },
+      "Lightning": { strength:89, lastMatches:[1,1,1] },
+      "Oilers": { strength:87, lastMatches:[0,0,1] }
     }
   }
-
 };
